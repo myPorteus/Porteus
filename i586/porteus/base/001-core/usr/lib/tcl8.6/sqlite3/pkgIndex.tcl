5e4986d770d1efd23abe498fe8d451e12e9ebdbb
hhlohmann <hhlohmann@yahoo.com>
2021-11-19 09:46:25 +0100

    Delete 3.2.2-i586 to prepare for 4.0

--- i586/porteus/base/001-core/usr/lib/tcl8.6/sqlite3/pkgIndex.tcl
+++
@@ -1,1 +0,0 @@
-package ifneeded sqlite3 3.13.0 [list load /usr/lib/tcl8.6/sqlite3/libtclsqlite3.so sqlite3]
--- i586/porteus/base/003-kde5/usr/bin/jemalloc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-prefix=/usr
-exec_prefix=/usr
-libdir=/usr/lib
-
-LD_PRELOAD=${libdir}/libjemalloc.so.1
-export LD_PRELOAD
-exec "$@"
